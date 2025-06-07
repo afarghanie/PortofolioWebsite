c0b544b27bcca4f3e179879e662f5c6eb708a6e5
afarghanie <alaviefarghanie@gmail.com>
2025-06-07 12:36:13 +0000

    commit -m "proffile"

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -12,7 +12,7 @@ const AboutSection: React.FC<AboutSectionProps> = ({ id, title, content }) => {
     <Section id={id} title={title} className="bg-[#1A1A24]"> {/* Updated background color */}
       <div className="text-center max-w-3xl mx-auto">
         <img
-          src="/assets/profilepicture.jpg" // Updated image path
+          src="assets/profile.jpg" // Updated image path
           alt="Profile"
           className="w-40 h-40 md:w-48 md:h-48 rounded-full mx-auto mb-8 shadow-xl border-4 border-[#00F0FF]" /* Updated border color */
         />
